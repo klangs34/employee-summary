c425a66b10677c03923faf7e3edfbdebcd9b7858
kelz <kelsie.langston@yahoo.com>
2020-01-22 21:53:37 -0500

    fix quirks

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const util = require('util');
 const fs = require("fs");
 const fsReadFileAsync = util.promisify(fs.readFile);
 const fsAppendFileAsync = util.promisify(fs.appendFile);
+const fsWriteFileAsync = util.promisify(fs.writeFile);
 
 const teamMembers = [];
 let question = "not done";
@@ -24,10 +25,11 @@ async function init() {
         if (!add) {
             question = "done";
         }
+
         //push team items in array
         teamMembers.push(teamData);
     }
-    
+
     //remove false/last object entry
     teamMembers.pop();
 
@@ -42,10 +44,10 @@ async function init() {
             let setId = setRole.replace("ID", `Id: ${manager.id}`);
             let mailtoEmail = setId.replace("mailto:Email", `mailto:${manager.email}`);
             let showEmail = mailtoEmail.replace("ShowEmail", `Email: ${manager.email}`);
-            let officeNum = showEmail.replace("OfficeNumber", `OfficeNumber: ${manager.getOfficeNumber()}`)
+            let officeNum = showEmail.replace("OfficeNumber", `Office Number: ${manager.getOfficeNumber()}`)
             return officeNum;
         }
-        
+
         let managerHtml = returnManagerData();
 
         //append manager data to main html
@@ -57,36 +59,37 @@ async function init() {
         let memberRole;
 
         //create an async function to read each data array
-        const readTemplateHtml = async function(obj) {
-            
-            if(obj.role === "Employee") {
+        const readTemplateHtml = async function (obj) {
+
+            if (obj.role === "Employee") {
                 //set template URL
                 templateURL = "./templates/employee.html";
             }
-            if(obj.role === "Engineer") {
+            if (obj.role === "Engineer") {
                 //set template URL
                 templateURL = "./templates/engineer.html";
             }
-            if(obj.role === "Intern") {
+            if (obj.role === "Intern") {
                 //set template URL
                 templateURL = "./templates/intern.html";
-            
+
             }
             //read each appropriate template
             const assignMemberHtml = await fsReadFileAsync(templateURL, "utf8");
 
             //create team member html file with replaced data
             let setRole = assignMemberHtml.replace("Name", `${obj.name}`);
-            let setId = setRole.replace("ID", `Id: ${manager.id}`);
-            let mailtoEmail = setId.replace("mailto:Email", `mailto:${manager.email}`);
-            let showEmail = mailtoEmail.replace("ShowEmail", `Email: ${manager.email}`);
-            if(obj.role === "Employee") {
+            let setId = setRole.replace("ID", `Id: ${obj.id}`);
+            let mailtoEmail = setId.replace("mailto:Email", `mailto:${obj.email}`);
+            let showEmail = mailtoEmail.replace("ShowEmail", `Email: ${obj.email}`);
+            if (obj.role === "Employee") {
                 memberRole = showEmail.replace("Title", `Title: ${obj.title}`);
             }
-            if(obj.role === "Engineer"){
-                memberRole = showEmail.replace("GitHub", `Github: ${obj.github}`);
+            if (obj.role === "Engineer") {
+                memberRole = showEmail.replace("github.com/GitHub", `http://github.com/${obj.github}`);
+                memberRole = memberRole.replace("GitHub", `Github: ${obj.github}`);
             }
-            if(obj.role === "Intern") {
+            if (obj.role === "Intern") {
                 memberRole = showEmail.replace("School", `School: ${obj.school}`);
             }
 
@@ -94,22 +97,23 @@ async function init() {
         }
 
         //append each team member div to the main html file
-        teamMembers.forEach(async val => {
-            let teamData = await readTemplateHtml(val);
-            let i = 0;
-            //if last element is past, add closing elements
-            if(i === teamMembers.length - 1) {
-                await fsAppendFileAsync('./templates/main.html', teamData + "</body></html>");
-            } else {
-                await fsAppendFileAsync('./templates/main.html', teamData);
-                i++;
-            }
-        })
-            
+        for (let i = 0; i < teamMembers.length; i++) {
+            let teamData = await readTemplateHtml(teamMembers[i]);
+            await fsAppendFileAsync('./templates/main.html', teamData);
+        }
+
     } catch (error) {
         console.log("message: " + error);
     }
 
 }
 
-init();
+async function startCLI() {
+    await init();
+    //read main html file
+    const data = await fsReadFileAsync('./templates/main.html', "utf8");
+    //write main file contents in output file
+    await fsWriteFileAsync('./output/team.html', data + "</body></html>");
+}
+
+startCLI();
